d4927204c2826d1609760ea0654da367dcee6c6c
TakJewon <taknineball@castis.com>
2024-04-14 22:34:29 +0900

    asset.load 시도해봄

--- takPlay/ViewController.swift
+++ takPlay/ViewController.swift
@@ -6,13 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        
+        Task {
+          try await loadAsset()
+        }
     }
+    
+    func loadAsset() async throws{
+        print("loadasset!")
+        let asset = AVAsset(url: URL(string: "https://gist.github.com/SeunghoonBaek/f35e0fd3db80bf55c2707cae5d0f7184")!)
+        
+        // 2024-04-14 이것도 안되네
+
+        let (duration, tracks) = try await asset.load(.duration, .tracks)
+        
+        print("duration : %@", duration)
+        print("tracks : %@", tracks)
+    }
+    
 
 
 }
