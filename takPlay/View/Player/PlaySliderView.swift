e1b58b24f374dc22f7aa182bdb02920a27b49c49
TakJewon <taknineball@castis.com>
2024-04-27 00:10:34 +0900

    playSliderView 기능 연동, 버튼 선택에 따라 toolView 달라지도록 연동 필요

--- takPlay/View/Player/PlaySliderView.swift
+++ takPlay/View/Player/PlaySliderView.swift
@@ -34,7 +34,6 @@ class PlaySliderView: UIView, playerDeletegate {
         }
     }
     
-    
     // MARK: - override
     
     override init(frame: CGRect) {
--- takPlay/View/Player/PlayerViewController.swift
+++ takPlay/View/Player/PlayerViewController.swift
@@ -9,15 +9,14 @@ import UIKit
 import AVKit
 import Photos
 
-class PlayerViewController: UIViewController  {
+protocol playerDeletegate {
+    func settingSliderValue(min: Float, max: Float)
+}
+
+class PlayerViewController: UIViewController, playerSliderDeletegate  {
     
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
-    
-    @IBOutlet weak var runningTimeLabel: UILabel!
-    @IBOutlet weak var endTimeLabel: UILabel!
-    @IBOutlet weak var playSlider: UISlider!
-    @IBOutlet weak var playButton: UIButton!
     
     @IBOutlet weak var videoToolButton: UIButton!
     @IBOutlet weak var adjustToolButton: UIButton!
@@ -25,20 +24,12 @@ class PlayerViewController: UIViewController  {
     @IBOutlet weak var cropToolButton: UIButton!
     
     @IBOutlet weak var playerView: UIView!
+    @IBOutlet weak var toolView: UIView!
+    
     var player = AVPlayer()
-    
     var playUrl: URL?
-    var playerItem: AVPlayerItem!
     
-    var isPlaying: Bool = false {
-        didSet {
-            let imageName = isPlaying ? "pause.fill" : "play.fill"
-            let image = UIImage(systemName: imageName)?
-                .withTintColor(.white, renderingMode: .alwaysOriginal)
-            playButton.setImage(image, for: .normal)
-        }
-    }
-    
+    var delegate: playerDeletegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,25 +46,15 @@ class PlayerViewController: UIViewController  {
     
     func settingView(){
         
-        isPlaying = false
-        
-        // 원하는 이미지 크기와 색상으로 이미지 생성
-        let originalImage = UIImage(systemName: "rectangle.portrait.fill")?
-            .withTintColor(.white, renderingMode: .alwaysOriginal)
-
-        // 원하는 크기로 이미지 리사이징
-        let newSize = CGSize(width: 10, height: 50)
-        let resizedImage = UIGraphicsImageRenderer(size: newSize).image { _ in
-            originalImage?.draw(in: CGRect(origin: .zero, size: newSize))
+        guard let loadedNib = Bundle.main.loadNibNamed("PlaySliderView", owner: self, options: nil),
+              let playSliderView = loadedNib.first as? PlaySliderView else {
+            return
         }
-        self.playSlider.setThumbImage(resizedImage, for: .normal)
         
-        self.playSlider.minimumTrackTintColor = .white
-        self.playSlider.setValue(0, animated: false)
-        
-//        self.playSlider.layer.borderColor = CGColor(red: 255, green: 255, blue: 255, alpha: 1)
-//        self.playSlider.layer.borderWidth = 5
-        
+        playSliderView.initWithURL(playUrl: self.playUrl!, with: self.player)
+        playSliderView.delegate = self
+        self.delegate = playSliderView.self
+        self.toolView.addSubview(playSliderView)
         
         let videoSelectedImage = UIImage(systemName: "video.fill")?
             .withTintColor(.white, renderingMode: .alwaysOriginal)
@@ -115,55 +96,9 @@ class PlayerViewController: UIViewController  {
         playerLayer.frame = self.playerView.bounds
         playerLayer.videoGravity = .resizeAspect
         self.playerView.layer.addSublayer(playerLayer)
-        
-        //썸네일 생성
-        let asset = AVAsset(url: playUrl ?? defaultUrl!)
-        playerItem = AVPlayerItem(asset: asset)
-        
-        let thumbnailTime = CMTime(seconds: 1, preferredTimescale: 1)
-        generateThumbnail(for: playerItem, at: thumbnailTime, completion: { image in
-            let newSize = CGSize(width: 100, height: 50)
-            if let thumbnailImage = image?.resized(forHeight: 50)?.resizableImage(withCapInsets: .zero, resizingMode: .tile) {
-                DispatchQueue.main.async {
-                    self.playSlider.setMaximumTrackImage(thumbnailImage, for: .normal)
-                    self.playSlider.setMinimumTrackImage(thumbnailImage, for: .normal)
-                }
-            }
-        })
     
         // 재생 상태를 확인하고, 재생 가능한 상태가 되면 재생 시간을 설정합니다.
         item.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: [.new], context: nil)
-        
-        self.playSlider.addTarget(self, action: #selector(changeValue), for: .valueChanged)
-        
-        let interval = CMTimeMakeWithSeconds(1, preferredTimescale: Int32(NSEC_PER_SEC))
-        self.player.addPeriodicTimeObserver(forInterval: interval, queue: .main, using: { [weak self] elapsedSeconds in
-            let elapsedTimeSecondsFloat = CMTimeGetSeconds(elapsedSeconds)
-            let totalTimeSecondsFloat = CMTimeGetSeconds(self?.player.currentItem?.duration ?? CMTime(value: 1, timescale: 1))
-            print(elapsedTimeSecondsFloat, totalTimeSecondsFloat)
-            
-            // 현재 시간을 표시합니다.
-            let elapsedTime = CMTimeGetSeconds(self?.player.currentTime() ?? CMTime(value: 0, timescale: 1))
-            self?.runningTimeLabel.text = self?.formatTime(seconds: elapsedTime)
-            
-            // 슬라이더 위치를 조정합니다.
-            self?.playSlider.value = Float(elapsedTime)
-        })
-    }
-    
-    func generateThumbnail(for playerItem: AVPlayerItem, at time: CMTime, completion: @escaping (UIImage?) -> Void) {
-        let imageGenerator = AVAssetImageGenerator(asset: playerItem.asset)
-        imageGenerator.appliesPreferredTrackTransform = true
-        
-        imageGenerator.generateCGImagesAsynchronously(forTimes: [NSValue(time: time)], completionHandler: { requestedTime, cgImage, actualTime, result, error in
-            if let cgImage = cgImage {
-                let thumbnailImage = UIImage(cgImage: cgImage)
-                completion(thumbnailImage)
-            } else {
-                completion(nil)
-            }
-        })
-        
     }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
@@ -172,49 +107,31 @@ class PlayerViewController: UIViewController  {
                 let status = AVPlayerItem.Status(rawValue: statusNumber)
                 if status == .readyToPlay {
                     if let item = self.player.currentItem {
-                        self.playSlider.minimumValue = 0
-                        self.playSlider.maximumValue = Float(CMTimeGetSeconds(item.duration))
-                        self.endTimeLabel.text = formatTime(seconds: CMTimeGetSeconds(item.duration))
+                        self.delegate?.settingSliderValue(min: 0, max: Float(CMTimeGetSeconds(item.duration)))
                     }
                 }
             }
         }
     }
     
-    // 시간을 포맷하는 메서드
-    func formatTime(seconds: Double) -> String {
-        let formatter = DateComponentsFormatter()
-        formatter.allowedUnits = [.hour, .minute, .second]
-        formatter.unitsStyle = .positional
-        formatter.zeroFormattingBehavior = .pad
-        if let formattedString = formatter.string(from: seconds) {
-            // 시간이 0으로 표시되는 경우에는 시간 부분을 없애고 반환합니다.
-            if formattedString.starts(with: "00:") {
-                let index = formattedString.index(formattedString.startIndex, offsetBy: 3)
-                return String(formattedString[index...])
-            } else {
-                return formattedString
-            }
-        } else {
-            return "00:00"
-        }
-    }
+    //MARK: playerSliderDeletegate
     
-    
-    @objc private func changeValue(){
-        self.player.seek(to: CMTime(seconds: Double(self.playSlider.value), preferredTimescale: Int32(NSEC_PER_SEC)), completionHandler: { _ in
+    func changePlayerValue(value: Double) {
+        self.player.seek(to: CMTime(seconds: value, preferredTimescale: Int32(NSEC_PER_SEC)), completionHandler: { _ in
             
         })
     }
     
-    @IBAction func onTouchedPlayButton(_ sender: Any) {
-        if isPlaying {
+    func changePlayerStatus(by isPlaying: Bool) {
+        print("isPlaying ? \(isPlaying)")
+        if (isPlaying){
             self.player.pause()
         } else {
             self.player.play()
         }
-        isPlaying = !isPlaying
     }
+    
+    //MARK: IBAction
     
     @IBAction func onTapBackButton(_ sender: Any) {
         
@@ -226,6 +143,8 @@ class PlayerViewController: UIViewController  {
         self.adjustToolButton.isSelected = false
         self.filtersToolButton.isSelected = false
         self.cropToolButton.isSelected = false
+        
+        self.toolView.isHidden = false
     }
     
     
@@ -234,6 +153,8 @@ class PlayerViewController: UIViewController  {
         self.adjustToolButton.isSelected = true
         self.filtersToolButton.isSelected = false
         self.cropToolButton.isSelected = false
+        
+        self.toolView.isHidden = true
     }
     
     
@@ -242,6 +163,8 @@ class PlayerViewController: UIViewController  {
         self.adjustToolButton.isSelected = false
         self.filtersToolButton.isSelected = true
         self.cropToolButton.isSelected = false
+        
+        self.toolView.isHidden = true
     }
     
     
@@ -250,6 +173,8 @@ class PlayerViewController: UIViewController  {
         self.adjustToolButton.isSelected = false
         self.filtersToolButton.isSelected = false
         self.cropToolButton.isSelected = true
+        
+        self.toolView.isHidden = true
     }
     
     @IBAction func onTapSaveButton(_ sender: Any) {
@@ -275,6 +200,8 @@ class PlayerViewController: UIViewController  {
             }
         }
     }
+    
+    //MARK: functions
     
     func captureCurrentFrame(){
         
